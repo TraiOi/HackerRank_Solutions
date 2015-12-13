756d0e86e26441ef8f10c3cc7adb9965fb31dbdd
TraiOi <rayb.1002@gmail.com>
2015-12-14 03:31:31 +0700

    Updated

--- Ruby/01-Introduction/02.Everything_is_an_Object.rb
+++ Ruby/01-Introduction/02.Everything_is_an_Object.rb
@@ -1,9 +1,6 @@
 # Everything is an object in Ruby. For instance, if you type
-
 # print self
-
 # in the code-editor, Ruby treats self as the object in which it is currently referred to.
-
 # Give it a try!
 
 print self
